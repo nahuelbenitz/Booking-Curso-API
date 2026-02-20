e11a39385001e9b11ad8cf497cf5e97ab5939131
nahuelbenitz <bnahuel83@gmail.com>
2026-02-20 13:56:16 -0300

    Add project files.

---
+++ src/Booking.Domain/Entities/User/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Booking.Domain.Entities.User
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
